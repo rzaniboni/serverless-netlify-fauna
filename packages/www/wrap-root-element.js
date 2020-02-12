b5e8ea02ebf047d671f18fc8e1055cd25fa1ca50
Roberto Zaniboni <zaniboni@gmail.com>
2020-02-12 13:53:16 +0100

    fix: packages in node_modules

--- packages/www/wrap-root-element.js
+++ packages/www/wrap-root-element.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const { ThemeProvider } = require('theme-ui');
 const { deep } = require('@theme-ui/presets');
+
 // const { Provider } = require('./identity-context');
 
 const tokens = {
